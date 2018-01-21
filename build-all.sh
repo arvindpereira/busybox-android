1a60784b9f5b3b70260420bedb6a380625c4a23f
Arvind Pereira <arvind@markovcorp.com>
2018-01-20 18:17:10 -0800

    Initial fixes to get the build started. Not complete yet though because the build is still broken.

--- build-all.sh
+++ build-all.sh
@@ -5,9 +5,19 @@ NDK=`dirname $NDK`
 NDK=`readlink -f $NDK`
 
 export CFLAGS="-Os"
-./build.sh $NDK x86-4.8 x86 4.8 android-14 `pwd`/build/x86 || exit 1
-./build.sh $NDK arm-linux-androideabi-4.8 armeabi 4.8 android-14 `pwd`/build/arm || exit 1
-./build.sh $NDK mipsel-linux-android-4.8 mips 4.8 android-14 `pwd`/build/mips || exit 1
+
+# Find the toolchains
+i686path=`find $NDK -name i686-linux-android-gcc`
+i686path=`dirname $i686path`
+armpath=`find $NDK -name arm-linux-androideabi`
+armpath=`dirname $armpath`
+mipspath=`find $NDK -name mipsel-linux-android`
+mipspath=`dirname $mipspath`
+
+export PATH="${i686path}":"${armpath}":"${mipspath}":$PATH
+./build.sh $NDK x86-4.9 x86 4.9 android-14 `pwd`/build/x86 || exit 1
+./build.sh $NDK arm-linux-androideabi-4.9 armeabi 4.9 android-14 `pwd`/build/arm || exit 1
+#./build.sh $NDK mipsel-linux-android-4.8 mips 4.8 android-14 `pwd`/build/mips || exit 1
 
 #export CFLAGS="-Os -flto"
 #export LDFLAGS="-flto"
